a76ceb136bd2ca7346974bb19a8c88bb8118f362
Nefee1 <katalitika@mail.ru>
2023-07-16 17:16:50 +0700

    add Main исправленный

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,7 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
 
 public class Main {
-    public Main() {
-    }
-
     public static void main(String[] args) {
         task1();
         task2();
@@ -12,75 +11,89 @@ public class Main {
 
     public static void task1() {
         System.out.println("Задача 1");
-        int[] arraySize = new int[3];
-        arraySize[0] = 1;
-        arraySize[1] = 2;
-        arraySize[2] = 3;
-
-        float[] Size = {1.57f, 7.654f, 9.986f};
-
-        int[] arrayNumber = new int[5];
-        arrayNumber[0] = 157;
-        arrayNumber[1] = 55;
-        arrayNumber[3] = 5196;
-        arrayNumber[4] = 8198;
+        int[] arraySize = new int[]{1, 2, 3};
+        double[] Size = {1.57, 7.654, 9.986};
+        int[] arrayNumber = new int[]{15, 55, 0, 5196, 8198};
     }
 
     public static void task2() {
         System.out.println("Задача 2");
-        int[] arraySize = new int[3];
-        arraySize[0] = 1;
-        arraySize[1] = 2;
-        arraySize[2] = 3;
+        int[] arraySize = new int[]{1, 2, 3};
+        double[] Size = {1.57, 7.654, 9.986};
+        int[] arrayNumber = new int[]{15, 55, 0, 5196, 8198};
+        for (int i = 0; i < arraySize.length; i++) {
+            System.out.print(arraySize[i]);
 
-        float[] Size = {1.57f, 7.654f, 9.986f};
-        Size[0] = 1.57f;
-        Size[1] = 7.654f;
-        Size[2] = 9.986f;
+            if (i != arraySize.length - 1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
 
-        int[] Number = new int[5];
-        Number[0] = 157;
-        Number[1] = 55;
-        Number[3] = 5196;
-        Number[4] = 8198;
-        System.out.println(" 1 Массив " + arraySize[0] + "," + arraySize[1] + "," + arraySize[2]);
-        System.out.println(" 2 Массив " + Size[0] + "," + Size[1] + "," + Size[2]);
-        System.out.println(" 3 Массив " + Number[0] + "," + Number[1] + "," + Number[3] + "." + Number[4]);
+        for (int i = 0; i < Size.length; i++) {
+            System.out.print(Size[i]);
+
+            if (i != Size.length - 1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
+
+        for (int i = 0; i < arrayNumber.length; i++) {
+            System.out.print(arrayNumber[i]);
+
+            if (i != arrayNumber.length - 1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
     }
 
     public static void task3() {
         System.out.println("Задача 3");
-        int[] arraySize = new int[3];
-        arraySize[0] = 1;
-        arraySize[1] = 2;
-        arraySize[2] = 3;
+        int[] arraySize = new int[]{1, 2, 3};
+        double[] Size = {1.57, 7.654, 9.986};
+        int[] arrayNumber = new int[]{15, 55, 0, 5196, 8198};
+        for (int i = arraySize.length - 1; i >= 0; i--) {
+            System.out.print(arraySize[i]);
 
-        float[] Size = {1.57f, 7.654f, 9.986f};
-        Size[0] = 1.57f;
-        Size[1] = 7.654f;
-        Size[2] = 9.986f;
+            if (i != 0) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
 
-        int[] Number = new int[5];
-        Number[0] = 157;
-        Number[1] = 55;
-        Number[3] = 5196;
-        Number[4] = 8198;
-        System.out.println(" 1 Массив " + arraySize[2] + "," + arraySize[1] + "," + arraySize[0]);
-        System.out.println(" 2 Массив " + Size[2] + "," + Size[1] + "," + Size[0]);
-        System.out.println(" 3 Массив " + Number[4] + "," + Number[3] + "," + Number[1] + "." + Number[0]);
+        for (int i = Size.length - 1; i >= 0; i--) {
+            System.out.print(Size[i]);
+
+            if (i != 0) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
+
+        for (int i = arrayNumber.length - 1; i >= 0; i--) {
+            System.out.print(arrayNumber[i]);
+
+            if (i != 0) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
     }
-
     public static void task4() {
         System.out.println("Задача 4");
-        int[] arraySize = new int[3];
-        arraySize[0] = 1;
-        arraySize[1] = 2;
-        arraySize[2] = 3;
-        for (int i = 0; i < 3; i++) {
-            System.out.println(arraySize[i]);
-            arraySize[0] += 1;
-            arraySize[1] += 1;
-            arraySize[2] += 1;// Учитель я не понял как добавить +1 внуторь массива
+        int[] arraySize = new int[]{1, 2, 3};
+        double[] Size = {1.57, 7.654, 9.986};
+        int[] arrayNumber = new int[]{15, 55, 0, 5196, 8198};
+
+        for (int i = 0; i < arraySize.length; i++) {
+
+            if (arraySize[i] % 2 != 0) {
+                arraySize[i]++;
+            }
         }
+        System.out.println(arraySize);
+        System.out.println(Arrays.toString(arraySize));
     }
 }
